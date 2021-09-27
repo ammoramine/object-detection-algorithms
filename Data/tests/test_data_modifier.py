2f07d4e0eb9361c346d4856415e2d1d4119db190
Amine AMMOR <aamineammor@gmail.com>
2021-09-27 14:01:53 +0200

    renamed detections_per_class_filtering to label_filtering on the  Data/data_modifier folder

--- Data/tests/test_data_modifier.py
+++ Data/tests/test_data_modifier.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd,numpy as np
 
-from ..data_modifier import detections_per_class_filtering
+from ..data_modifier import label_filtering
 from .. import data_manager
 
 # pytest.mark.thisone
@@ -9,7 +9,7 @@ def test_offline_data_filtering():
     #TODO: add multiple combinations of classnames:
     classes_names = ["Bus", "Truck"]
     for mode in ["train","test","validation"]:
-        detections_per_class_filtering.rewrite_detections_csv_after_filtering(mode,classes_names = classes_names)
+        label_filtering.rewrite_detections_csv_after_filtering(mode, classes_names = classes_names)
 
         path_csv = data_manager.get_path_detection_csv_filtered(mode)
 
