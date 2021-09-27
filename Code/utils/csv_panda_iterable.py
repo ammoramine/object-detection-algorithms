d4f4c400f8b47a0221c793652673c754a7733f67
Amine AMMOR <aamineammor@gmail.com>
2021-09-27 13:26:31 +0200

    commented scripts on utils

---
+++ Code/utils/csv_panda_iterable.py
@@ -0,0 +1,10 @@
+# """
+#     a function that iterates over large csv file,
+#     can create large new csv file
+#     takes a condition func to filter the rows
+#
+#     This function would be used on a pytorch dataset, to feed neural network
+# """
+# import pandas as pd
+#
+# #TODO: read about the TextFileReader class
---
+++ Code/utils/utils_routines.py
@@ -0,0 +1,5 @@
+# from ...Data import
+#
+# def get_code_from_labels(mode,labels_names):
+#     cls = CodeLabelCorres(mode)
+#     return cls.get_code_from_labels(labels_names)
