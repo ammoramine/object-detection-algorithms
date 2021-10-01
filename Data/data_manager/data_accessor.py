f33da562972256953cbad1b01fad5e4a5a69c77a
Amine AMMOR <aamineammor@gmail.com>
2021-10-01 16:27:01 +0200

    enriched the dataaccesor, with new reading utilities, add utils_data_reading for reading of dataFrame with custom objects

--- Data/data_manager/data_accessor.py
+++ Data/data_manager/data_accessor.py
@@ -3,9 +3,10 @@ import pandas as pd
 from PIL import Image
 
 try:
-    from . import data_manager
+    from .. import data_manager
+    from .. import utils_data_reading
 except:
-    from Data import data_manager
+    from Data import data_manager,utils_data_reading
 
 
 class DataAccessor:
@@ -27,6 +28,32 @@ class DataAccessor:
         # TODO: add python script to create from datasetRCNN, a new csv file, offline,or to
         # or to naviguate trought a large csv file
         return pd.read_csv(self.path_to_labels_csv)
+
+    def read_data_prepared_for_RCNN(self):
+        """
+        read csv file, containing data directly used by the neural network of the RCNN model
+        :return: data represented as a dataframe
+        """
+        path_csv = data_manager.get_path_detections_csv_for_RCNN_dataset(self.mode)
+        assert path_csv.exists(),f"should launch data_preparer_dataset_RCNN.py for mode {self.mode}"
+        df = utils_data_reading.read_csv_and_eval(path_csv,collumns_eval=["p_bboxes",'gd_bboxes','labels','offsets'])
+        return df
+
+    def read_data_of_region_crs(self,truncate=None):
+        """
+        csv file, containing correspondances, between bbox extracted with a region proposal
+        algorithm, and labeelled bboxes
+        :param truncate: if int read only the first "truncate" rows,if None, read all the lines
+        :return:
+        """
+
+        path_csv = data_manager.get_path_detecion_csv_filtered_with_rpropos(self.mode)
+        assert path_csv.exists(),f"should launch data_preparer_dataset_RCNN.py for mode {self.mode}"
+
+        res = utils_data_reading.read_rpos_csv(path_csv,truncate)
+
+        res = pd.DataFrame(res,columns=["imageID","p_bboxes","gd_bboxes","labels_gd_bboxes"])
+        return res
 
     def get_pil_image_from_name(self, image_id):
         path_img = self.path_image_folder.joinpath(image_id + ".jpg")
