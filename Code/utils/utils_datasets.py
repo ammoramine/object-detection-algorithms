edba0930a3f6307bc1ba511935d55bcb323caef8
Amine AMMOR <aamineammor@gmail.com>
2021-10-04 13:45:28 +0200

    tiny module to be filled with usefull functions later

--- Code/utils/utils_datasets.py
+++ Code/utils/utils_datasets.py
@@ -1,5 +1,10 @@
-# from ...Data import
-#
-# def get_code_from_labels(mode,labels_names):
-#     cls = CodeLabelCorres(mode)
-#     return cls.get_code_from_labels(labels_names)
+try:
+    from ..datasets import dataset_YOLO
+except:
+    from Code.datasets import dataset_YOLO
+
+DatasetYOLO = dataset_YOLO.DatasetYOLO
+
+def get_data_set_yolo(**kwargs):
+    dataset = dataset_YOLO.DatasetYOLO(**kwargs)
+    return dataset
