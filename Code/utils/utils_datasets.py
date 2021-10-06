ea79d636f002ebcbb6e9c9bd21ba55d97158787d
Amine AMMOR <aamineammor@gmail.com>
2021-10-06 11:05:22 +0200

    added transforms to truncate dataset, for a limited number of samples

--- Code/utils/utils_datasets.py
+++ Code/utils/utils_datasets.py
@@ -3,7 +3,13 @@ try:
 except:
     from Code.datasets import dataset_YOLO
 
+import torch
+
 
 def get_data_set_yolo(**kwargs):
     dataset = dataset_YOLO.DatasetYOLO(**kwargs)
+    return dataset
+
+def truncate_dataset(dataset,N):
+    dataset = torch.utils.data.random_split(dataset, lengths=[N, len(dataset) - N])[0]
     return dataset
